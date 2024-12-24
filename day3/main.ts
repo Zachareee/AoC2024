fd9c442a75b5bcaee5f51189697a3ff37de0547a
Zachareee <zacharytanhx@yahoo.com>
2024-12-25 01:14:27 +0800

    Added ts answer for q3p2

--- day3/main.ts
+++ day3/main.ts
@@ -1,17 +1,25 @@
 import fs from "node:fs";
-function answer1(lines: string[]): number {
-  return lines.map((line) =>
-    line.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)
-      .map((match) =>
-        match.slice(1).reduce((prev, curr) => prev * parseInt(curr), 1)
-      )
-      .reduce((prev, curr) => prev + curr)
-  ).reduce((prev, curr) => prev + curr);
+const add = (num1: number, num2: number) => num1 + num2;
+function answer1(line: string): number {
+  return line.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)
+    .map((match) =>
+      match.slice(1).reduce((prev, curr) => prev * parseInt(curr), 1)
+    )
+    .reduce(add, 0);
+}
+function answer2(lines: string): number {
+  return answer1(lines) -
+    // find the don't()...do() blocks and subtract them
+    lines.match(/don't\(\).*?do\(\)/g)!.map(answer1).reduce(add, 0) -
+    // find the unclosed don't() block
+    lines.match(/don't\(\)(?!.*do\(\)).*/g)!.map(answer1).reduce(add, 0);
 }
 function start() {
-  const input = fs.readFileSync("input.txt", { encoding: "ascii" });
-  const lines = input.split(/\r?\n/);
-  lines.pop();
+  const lines = fs.readFileSync("input.txt", { encoding: "ascii" }).replaceAll(
+    /\r?\n/g,
+    "",
+  );
   console.log(answer1(lines));
+  console.log(answer2(lines));
 }
 start();
